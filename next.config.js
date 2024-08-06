d3f542d97dd48ee31489b63c42c427496de6b5b2
Rauno Kaldmaa <rauno.tabularasa@gmail.com>
2024-08-06 16:33:47 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/discussion-page",
-  output: "export",
   images: {
     domains: ['picky-app.s3-ap-southeast-1.amazonaws.com'],
   },
